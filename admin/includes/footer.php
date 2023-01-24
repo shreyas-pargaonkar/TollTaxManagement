eceb7619116ed05247307b5278a1f195dd114359
shreyas-pargaonkar <shreyaspargaonkar@gmail.com>
2023-01-24 02:58:10 -0500

    dbms project

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copy_layout login">
+      <p style="color: black;font-size: 15px"> </p>
+   </div>
---
+++ staffs/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copy_layout login">
+      <p style="color: black;font-size: 15px">Toll Tax Management System  </p>
+   </div>
